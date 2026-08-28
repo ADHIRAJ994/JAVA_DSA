cf89dbc3c9b729f87cceef18120b3a71f3d864fc
ADHIRAJ994 <youradhi20@gmail.com>
2026-08-28 21:00:39 +0530

    Done Three_sum and four_Sum

--- Arrayss/Arrays_.java
+++ Arrayss/Arrays_.java
@@ -319,10 +319,86 @@ public static List<Integer> getRow(int rowIndex) { // LC:119
         }
         return ansRow;
 }
+public static List<Integer> MajorityElement2(int arr[]){
+    int n = arr.length;
+    List<Integer> ans = new ArrayList<>();
+    Map<Integer,Integer> mpp = new HashMap<>();
+    int min = (n/3)+1;
+    for(int i = 0;i<n;i++){
+        mpp.put(arr[i],mpp.getOrDefault(arr[i],0)+1);
+        if(mpp.get(arr[i])==min){
+            ans.add(arr[i]);
+        }
+    }
+    return ans;
+}
+public static List<List<Integer>>Three_Sum(int nums[]){
+    int n = nums.length;
+    List<List<Integer>> ans = new ArrayList<>();
+    Arrays.sort(nums);
+    for(int i = 0;i<n;i++){
+        if(i>0&&nums[i]==nums[i-1]) continue;
+        int j = i+1;
+        int k = n-1;
+        while(j<k){
+            int sum = nums[i]+nums[j]+nums[k];
+            if(sum<0){
+                j++;
+            }else if(sum>0){
+                k--;
+            }else{
+                List<Integer> valid = new ArrayList<>();
+                valid.add(nums[i]);
+                valid.add(nums[j]);
+                valid.add(nums[k]);
+                ans.add(valid);
+                j++;
+                k--;
+                while(j<k&&nums[j]==nums[j-1]) j++;
+                while(j<k&&nums[k]==nums[k+1]) k--;
+            }
+        }
+    }
+    return ans;
+}
+public static List<List<Integer>> fourSum(int nums[],int target){
+    List<List<Integer>> ans = new ArrayList<>();
+    Arrays.sort(nums);
+    int n = nums.length;
+    for(int i = 0;i<n;i++){
+        if(i>0&&nums[i]==nums[i-1]) continue;
+        for(int j = i+1;j<n;j++){
+            if(j!=i+1&&nums[j]==nums[j-1]) continue;
+            int k = j+1;
+            int l = n-1;
+            while(k<l){
+                long sum = nums[i];
+                sum+=nums[j];
+                sum+=nums[k];
+                sum+=nums[l];
+                if(sum==target){
+                    List<Integer> valid = new ArrayList<>();
+                    valid.add(nums[i]);
+                    valid.add(nums[j]);
+                    valid.add(nums[k]);
+                    valid.add(nums[l]);
+                    ans.add(valid);
+                    k++;
+                    l--;
+                    while(k<l&&nums[k]==nums[k-1]) k++;
+                    while(k<l&&nums[l]==nums[l+1]) l++;
+                }else if(sum<target) k++;
+                else l--;
+            }
+        }
+    }
+    return ans;
+}
     public static void main(String[] args){
         int arr[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        int arr1[] = {1,4,2,3,2,0,-1};
         int k = 4;
-        List<List<Integer>> ans = pascalTriangle(6);
+        List<List<Integer>> ans = fourSum(arr1,k);
         System.out.println(ans);
     }
 }
