a96bf569621a969bcd8a6d12cad3d34662ee094a
ADHIRAJ994 <youradhi20@gmail.com>
2026-09-15 20:28:00 +0530

    FindMin

--- Binary Search/search.java
+++ Binary Search/search.java
@@ -135,9 +135,24 @@ public static boolean Search2(int[] nums, int target) {
 
     return false;
 }
+public static int findMin(int nums[]){
+    int n = nums.length;
+    int low = 0,high=n-1,ans = Integer.MAX_VALUE;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(nums[low]<=nums[mid]){
+            ans = Math.min(ans,nums[low]);
+            low = mid+1;
+        }else{
+            ans = Math.min(ans,nums[mid]);
+            high = mid-1;
+        }
+    }
+    return ans;
+}
     public static void main(String args[]){
         int arr[] = {4,5,6,7,0,1,2};
-        int ans = Search(arr,0);
+        int ans = findMin(arr);
         System.out.println(ans);
     }
 }
